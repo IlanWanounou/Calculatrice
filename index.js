d4fa6ab8619c44b2e820c678232dbb636eabf311
Spinmeisterr <Spinmeisterr@users.noreply.github.com>
2021-03-20 01:57:58 +0100

    Ajout decimal

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-
 const touche = document.querySelector('.touche');
 const ecran = document.querySelector('.calculator-screen');
-
+const operator = document.querySelector('.operator');
+ecran.value = 0;
+let isDecimal=false;
 touche.addEventListener('click', e => {
   const affiche = e.target;
   if (!affiche.matches('button')) {
     return;
   }
-  if(affiche.matches(".operator")||affiche.matches(".plus")||affiche.matches(".egal")||affiche.matches(".decimal")) {
+
+  if (ecran.value === '0' && affiche.matches(".operator") || affiche.matches(".egal")) {
     return;
   }
+
   if (ecran.value === '0' && affiche.matches('.zero')) {
-    ecran.value = affiche.value;
-  } else {
-    ecran.value = parseInt(ecran.value + affiche.value);
+    return ecran.value = affiche.value;
+
   }
-});
+  if (affiche.classList.contains('decimal')) {
+VerifDecimal( affiche.value)
+return;
+  }
+  ecran.value = parseFloat(ecran.value + affiche.value);
+})
+function VerifDecimal(val) {
+  if (!isDecimal) {
+    ecran.value += val;
+    isDecimal=true;
+  }
+}
