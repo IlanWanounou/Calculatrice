7be7e12f65aba4688a491cf96abc8d0321b4c13c
Spinmeisterr <Spinmeisterr@users.noreply.github.com>
2021-03-21 17:34:43 +0100

    V1 stable

--- index.js
+++ index.js
@@ -1,41 +1,77 @@
 const touche = document.querySelector('.touche');
 const ecran = document.querySelector('.calculator-screen');
-ecran.value='0'
-
+let numB = "";
+let numA = "";
+let operation = "";
+let resultat = "";
 touche.addEventListener('click', e => {
   const affiche = e.target;
-  
+
   if (!affiche.matches('button')) {
     return;
   }
-
   if (ecran.value === '0' && affiche.matches('.zero')) {
     return ecran.value = affiche.value;
-  
-  }
-  if (affiche.classList.contains('decimal')) {
-    VerifDecimal( affiche.value)
-    return;
-      }
-      calcul(affiche.value)
-  
-})
 
+  } if (affiche.classList.contains('number')) {
+    //VerifDecimal(affiche.value)
+    numB += affiche.value;
+    ecran.value = numB;
+  } else calcul(affiche.value);
+
+});
 
 function calcul(val) {
-  if (val === '=') {
-
-      ecran.value = eval(ecran.value);
-  }else if(val==="AC") {
-    ecran.value="0";
-  }   else {
-    ecran.value += val;
+  if (val === "AC") {
+    ecran.value = "";
+    numA = "";
+    numB = "";
+    operation = "";
+    return;
   }
-}
+  else if (numA !== "") {
+    numB = parseFloat(numB);
+    switch (operation) {
+      case "/":
+        if (numB === 0) {
+          resultat = "Euh.";
+        } else {
+          resultat = numA / numB;
+        }
+        break;
+      case "*":
+        resultat = numA * numB;
+        break;
+      case "-":
+        resultat = numA - numB;
+        break;
+      case "+":
+        resultat = numA + numB;
+        break;
+      case "=":
+        resultat = parseFloat(ecran.value);
+        break;
 
-
-function VerifDecimal(val) {
+    };
+    if (resultat==="Euh.") {
+      setTimeout(function () {
+        numA = "";
+        numB = "";
+        operation = "";
+        ecran.value="";
+      }, 2000);
+    }
+      ecran.value = resultat;
+      numA = resultat;
+  } else if (numB !== "") {
+    numA = (numB);
+  } 
+  numB = "";
+  operation = val;
+};
+/*function VerifDecimal(val) {
   if (!ecran.value.includes('.')) {
     ecran.value += val;
   }
-}
+  return;
+}*/
